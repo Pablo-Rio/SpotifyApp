00df0ac9e3bca2cd43e1a4dfa7c14a167b366c5f
Paul Rivière <paulriviere002@gmail.com>
2023-03-22 16:51:53 +0100

    Add images and grid

---
+++ PaulSpotifyApp/ViewModels/PageUneViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Xamarin.Forms;
+
+namespace XXXXXX
+{
+    public class PageUneViewModel : BaseViewModel
+    {
+        #region Instance
+
+        public static PageUneViewModel Instance { get; } = new PageUneViewModel();
+
+        #endregion
+
+        public PageUneViewModel()
+        {
+            FirstColor = Color.GreenYellow;
+            SecondColor = Color.Red;
+        }
+
+        public Color FirstColor
+        {
+            get
+            {
+                return GetValue<Color>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+        public Color SecondColor
+        {
+            get
+            {
+                return GetValue<Color>();
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+
+        public void UpdateColors()
+        {
+            if (FirstColor == Color.GreenYellow)
+            {
+                FirstColor = Color.Red;
+                SecondColor = Color.GreenYellow;
+            }
+            else
+            {
+                SecondColor = Color.Red;
+                FirstColor = Color.GreenYellow;
+            }
+        }
+    }
+}
--- PaulSpotifyApp/Views/PageUne.xaml.cs
+++ PaulSpotifyApp/Views/PageUne.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XXXXXX;
 
 namespace PaulSpotifyApp.Views
 {
@@ -15,6 +16,12 @@ namespace PaulSpotifyApp.Views
         public PageUne()
         {
             InitializeComponent();
+            BindingContext = PageUneViewModel.Instance;
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            PageUneViewModel.Instance.UpdateColors();
         }
     }
 }
