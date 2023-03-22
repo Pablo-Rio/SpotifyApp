b4a8bc3bc530709f314d42309e613b0ec30c2c30
Paul Rivière <paulriviere002@gmail.com>
2023-03-22 16:08:18 +0100

    Add menu

--- PaulSpotifyApp/Views/PageTrois.xaml.cs
+++ PaulSpotifyApp/Views/PageTrois.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace PaulSpotifyApp
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaulSpotifyApp.Views
 {
-    public partial class MainPage : Shell
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageTrois : ContentPage
     {
-        public MainPage()
+        public PageTrois()
         {
             InitializeComponent();
         }
