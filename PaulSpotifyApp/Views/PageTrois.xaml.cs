6af038af8a0bc7366e2677f1c7ebc82f08167cbe
Paul Rivière <paulriviere002@gmail.com>
2023-04-05 21:49:47 +0200

    Add content of page 3

--- PaulSpotifyApp/Views/PageTrois.xaml.cs
+++ PaulSpotifyApp/Views/PageTrois.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PaulSpotifyApp.Service;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,22 @@ namespace PaulSpotifyApp.Views
         public PageTrois()
         {
             InitializeComponent();
+            var albumId = "4xnYue1MP5wspZzWyzEkmQ";
+
+            this.NomAlbum.Text = SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId)
+                .Result.Name;
+            this.ImageDeLAlbum.Source =
+                SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.Images[0].Url;
+            this.Artiste.Text = SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.Artists[0].Name;
+            this.DateDeSortie.Text = SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.ReleaseDate;
+            
+            var longueur = SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.Tracks.Items.Count;
+            string[] tableau = new string[longueur];
+            for (int i = 0; i < longueur; i++)
+            {
+                tableau[i] = SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.Tracks.Items[i].Name;
+            }
+            this.ListeDesTitres.ItemsSource = tableau;
         }
     }
 }
