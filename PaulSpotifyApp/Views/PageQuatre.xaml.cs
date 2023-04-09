61357f798b367c536bba537c62045463bb2a1f15
Paul Rivière <paulriviere002@gmail.com>
2023-04-09 21:30:05 +0200

    Add informations on Page 4

--- PaulSpotifyApp/Views/PageQuatre.xaml.cs
+++ PaulSpotifyApp/Views/PageQuatre.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PaulSpotifyApp.Service;
+using Plugin.SimpleAudioPlayer;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,38 @@ namespace PaulSpotifyApp.Views
         public PageQuatre()
         {
             InitializeComponent();
+            var trackId = "0pqnGHJpmpxLKifKRmU6WP";
+            
+            this.NomTitre.Text = SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId)
+                .Result.Name;
+            this.NomArtiste.Text = SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId)
+                .Result.Artists[0].Name;
+            this.NomAlbum.Text = "Album : " + SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId)
+                .Result.Album.Name;
+            this.ImageDeLAlbum.Source = SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId)
+                .Result.Album.Images[0].Url;
+            var albumId = SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId)
+                .Result.Album.Id;
+            this.NumeroDeLaPiste.Text = "Piste : " + SpotifyService.Instance.GetSpotifyClient().Tracks
+                .Get(trackId)
+                .Result.TrackNumber + " sur " + SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result
+                .Tracks.Items.Count;
+            var minutes = SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId).Result
+                .DurationMs / 1000 / 60;
+            var secondes = SpotifyService.Instance.GetSpotifyClient().Tracks.Get(trackId).Result
+                .DurationMs / 1000 % 60;
+            this.Duree.Text = "Durée : " + minutes + " min " + secondes + " s";
+            
+            // Durée totale de l'album
+            var longueur = SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.Tracks.Items.Count;
+            int dureeTotale = 0;
+            for (int i = 0; i < longueur; i++)
+            {
+                dureeTotale += SpotifyService.Instance.GetSpotifyClient().Albums.Get(albumId).Result.Tracks.Items[i].DurationMs;
+            }
+            var minutesTotales = dureeTotale / 1000 / 60;
+            var secondesTotales = dureeTotale / 1000 % 60;
+            this.DureeTotale.Text = "Durée totale de l'album : " + minutesTotales + " min " + secondesTotales + " s";
         }
     }
 }
