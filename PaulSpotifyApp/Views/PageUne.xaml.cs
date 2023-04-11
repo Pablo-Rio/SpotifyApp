f710d55fc194ae9c59937acf5ecb820d6744fdc4
Paul Rivière <paulriviere002@gmail.com>
2023-04-12 00:47:50 +0200

    Add letter Page 1

--- PaulSpotifyApp/Views/PageUne.xaml.cs
+++ PaulSpotifyApp/Views/PageUne.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XXXXXX;
@@ -19,9 +19,22 @@ namespace PaulSpotifyApp.Views
             BindingContext = PageUneViewModel.Instance;
         }
 
+
         private void Button_OnClicked(object sender, EventArgs e)
         {
             PageUneViewModel.Instance.UpdateColors();
+        }
+
+        private void OnImageTapped(object sender, EventArgs e)
+        {
+            Image fermee = this.FindByName<Image>("enveloppeF");
+            fermee.IsVisible = false;
+            Image ouverte = this.FindByName<Image>("enveloppeO");
+            ouverte.IsVisible = true;
+            Image lettre = this.FindByName<Image>("lettre");
+            lettre.IsVisible = true;
+            StackLayout tableau = this.FindByName<StackLayout>("tableau");
+            tableau.IsVisible = true;
         }
     }
 }
